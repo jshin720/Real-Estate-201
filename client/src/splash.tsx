989ae8625e03781d5b4429522193a303216ac7aa
Jan Daniel Fermin <jandanielfermin@gmail.com>
2022-06-08 14:38:32 -0700

    added darker shade to splash background

--- client/src/splash.tsx
+++ client/src/splash.tsx
@@ -6,8 +6,8 @@ const Splash = () => {
    
     return (
         <Container className="vw-100 vh-100 d-flex flex-column justify-content-center align-items-center" id="bg-cover" fluid>
-                <h1>Find Your New Home</h1>
-                <p>Connecting people and their future homes.</p>
+                <h1 className="display-1">Find Your New Home</h1>
+                <h3>Connecting people and their future homes.</h3>
         </Container>
     )
 };
