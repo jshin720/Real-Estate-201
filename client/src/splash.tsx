1b41f8ea0a65f6e78b1425c41cb498d1f79a06b5
Jan Daniel Fermin <jandanielfermin@gmail.com>
2022-06-08 14:39:35 -0700

    changed splash heading

--- client/src/splash.tsx
+++ client/src/splash.tsx
@@ -6,7 +6,7 @@ const Splash = () => {
    
     return (
         <Container className="vw-100 vh-100 d-flex flex-column justify-content-center align-items-center" id="bg-cover" fluid>
-                <h1 className="display-1">Find Your New Home</h1>
+                <h1 className="display-1">Find Your New Abode</h1>
                 <h3>Connecting people and their future homes.</h3>
         </Container>
     )
