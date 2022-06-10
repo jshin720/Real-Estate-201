4249dcfbd565ab0a78ad674415250342d66e0890
Jan Daniel Fermin <jandanielfermin@gmail.com>
2022-06-09 18:36:06 -0700

    added react bootstrap icons

--- client/src/splash.tsx
+++ client/src/splash.tsx
@@ -1,15 +1,24 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Form, FormControl, Button } from "react-bootstrap";
 import './splash.css'
 
 const Splash = () => {
-   
+
     return (
         <Container className="vw-100 vh-100 d-flex flex-column justify-content-center align-items-center" id="bg-cover" fluid>
-                <h1 className="display-1">Find Your New Abode</h1>
-                <h3>Connecting people and their future homes.</h3>
+            <h1 className="display-1">Find Your New Abode</h1>
+            <h3>Connecting people and their future homes.</h3>
+            <Form className="d-flex">
+                <FormControl
+                    type="search"
+                    placeholder="Search"
+                    className="me-2"
+                    aria-label="Search"
+                />
+                <Button variant="outline-success"></Button>
+            </Form>
         </Container>
     )
 };
- 
+
 export default Splash;
